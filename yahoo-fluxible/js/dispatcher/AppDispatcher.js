bf053aa1d1de628f3fc14727758a5d8375605dad
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2015-01-31 19:59:09 +0200

    remove dispatchr

--- yahoo-fluxible/js/dispatcher/AppDispatcher.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var Dispatchr = require('dispatchr')();
-
-var ProductStore = require('./stores/ProductStore');
-var CartStore = require('./stores/CartStore');
-
-Dispatchr.registerStore(ProductStore);
-Dispatchr.registerStore(CartStore);

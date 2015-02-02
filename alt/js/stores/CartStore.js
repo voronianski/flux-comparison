f3e57609d93e4859ab1dd62fa643f03b841b5046
Josh Perez <josh@goatslacker.com>
2015-02-02 15:38:44 -0800

    Adds alt implementation

---
+++ alt/js/stores/CartStore.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var alt = require('../alt');
+
+var ActionCreators = require('../actions/ActionCreators');
+var ProductStore = require('./ProductStore');
+var assign = require('object-assign');
+
+class CartStore {
+    constructor() {
+        this.bindActions(ActionCreators);
+        this.products = {};
+    }
+
+    onAddToCart(product) {
+        this.waitFor(ProductStore.dispatchToken);
+        var id = product.id;
+        product.quantity = id in this.products ? this.products[id].quantity + 1 : 1;
+        this.products[id] = assign({}, product[id], product);
+    }
+
+    onCartCheckout() {
+        this.products = {};
+    }
+
+    onFinishCheckout(products) {
+        // this can be used to redirect to success page, etc.
+        console.log('YOU BOUGHT:', products);
+    }
+
+    static getAddedProducts() {
+        var { products } = this.getState();
+        return Object.keys(products).map(function (id) {
+            return products[id];
+        });
+    }
+
+    static getTotal() {
+        var total = 0;
+        var { products } = this.getState();
+        for (var id in products) {
+            var product = products[id];
+            total += product.price * product.quantity;
+        }
+        return total.toFixed(2);
+    }
+}
+
+module.exports = alt.createStore(CartStore, 'CartStore');
